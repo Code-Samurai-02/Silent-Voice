cac377639933b185daf7c575db97ceb2f9aa8c28
SK SAHIL UDDIN <151388592+SAHIL-S02@users.noreply.github.com>
2026-01-16 20:50:26 +0530

    Update

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Silent-Voice
+
+
+ax = x axis 
+ay = y axis
+az = z axis
+
+ax, ay, az = mpu.get_accel()
+
+if ax > 0.2:
+    print("RIGHT")
+elif ax < -0.2:
+    print("LEFT")
+else:
+    print("CENTER")
--- tca9548a.py
+++ tca9548a.py
@@ -8,30 +8,18 @@ class TCA9548A:
         self.addr = addr
 
     def select(self, channel):
-        """
-        Select I2C channel (0–7)
-        """
         if channel < 0 or channel > 7:
             raise ValueError("Channel must be 0–7")
         self.i2c.writeto(self.addr, bytes([1 << channel]))
 
     def disable(self):
-        """
-        Disable all channels
-        """
         self.i2c.writeto(self.addr, b'\x00')
 
     def scan(self, channel):
-        """
-        Scan devices on a specific channel
-        """
         self.select(channel)
         return self.i2c.scan()
 
     def scan_all(self):
-        """
-        Scan all channels
-        """
         devices = {}
         for ch in range(8):
             self.select(ch)
