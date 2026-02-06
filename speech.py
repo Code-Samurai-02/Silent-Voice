921d5626f6b07be85a203b68cd6698c4095fe279
SK SAHIL UDDIN <151388592+SAHIL-S02@users.noreply.github.com>
2026-02-06 19:51:25 +0530

    Update

--- speech.py
+++ speech.py
@@ -2,12 +2,24 @@ import asyncio
 import edge_tts
 import os
 
-TEXT = "Silent Voice system is ready"
+TEXT1 = "I'm sahil"
+TEXT2 = "This is our project Silent voice"
+TEXT3 = "Thank You"
 VOICE = "en-US-AriaNeural"
 FILE = "speech.mp3"
 
 async def main():
-    await edge_tts.Communicate(TEXT, VOICE).save(FILE)
-    os.system(f'explorer.exe {FILE}')
+    a = int(input("-->"))
+    if(a == 1):
+        await edge_tts.Communicate(TEXT1, VOICE).save(FILE)
+        os.system(f'explorer.exe {FILE}')
+    elif (a == 2):
+        await edge_tts.Communicate(TEXT2, VOICE).save(FILE)
+        os.system(f'explorer.exe {FILE}')
+    elif(a == 3):
+        await edge_tts.Communicate(TEXT3, VOICE).save(FILE)
+        os.system(f'explorer.exe {FILE}')
+    
 
-asyncio.run(main())
+while True:
+    asyncio.run(main())
