4ee62508c27b978a2d2d3b3e199960ed2c2002e3
SK SAHIL UDDIN <151388592+SAHIL-S02@users.noreply.github.com>
2026-08-29 22:13:28 +0530

    Added voice

---
+++ version 1.2.0/tts/testing_voice_v1.1.py
@@ -0,0 +1,28 @@
+from kokoro import KPipeline
+import sounddevice as sd
+
+# Load model once
+pipeline = KPipeline(lang_code="a")
+
+print("Kokoro TTS ready.")
+print("Type text and press Enter. Type 'exit' to quit.\n")
+
+while True:
+    text = input("You: ").strip()
+
+    if text.lower() == "exit":
+        break
+
+    if not text:
+        continue
+
+    print("Speaking...")
+
+    for _, _, audio in pipeline(
+        text,
+        voice="af_heart"
+    ):
+        sd.play(audio, 24000)
+        sd.wait()
+
+print("TTS stopped.")
